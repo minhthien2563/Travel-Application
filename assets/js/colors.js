01488a7f93a14a002c31c8f51456e1110d919662
minhthien2563 <minhthien2563@gmail.com>
2023-08-29 08:12:15 +0700

    new assets

---
+++ assets/js/colors.js
@@ -0,0 +1,9 @@
+export default {
+  primary: '#2db4f2',
+  primary_light: '#d6f2ff',
+
+  grey: '#454A53',
+
+  google: '#4688F8',
+  facebook: '#3A5897',
+};
---
+++ assets/js/slides.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '1',
+    title: 'Make your own travel plan',
+    description: 'Formulate your strategy to receive wonderful gift packs',
+    image: require('../images/Discovery.png'),
+  },
+  {
+    id: '2',
+    title: 'Customize your High-end travel',
+    description: 'Countless high-end entertainment facilities',
+    image: require('../images/Floating.png'),
+  },
+  {
+    id: '3',
+    title: 'Leisure packages to choose from',
+    description: 'First-class modern leisure and entertainment travel packages',
+    image: require('../images/Window_seat.png'),
+  },
+];
