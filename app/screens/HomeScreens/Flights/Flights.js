80019a27087257194f00349fa37531599153a963
minhthien2563 <minhthien2563@gmail.com>
2023-08-30 10:27:36 +0700

    remove shadow container

--- app/screens/HomeScreens/Flights/Flights.js
+++ app/screens/HomeScreens/Flights/Flights.js
@@ -102,16 +102,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 20,
     borderRadius: 20,
-
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-
-    elevation: 2,
   },
   boldText: {
     color: 'black',
